0cc1a3b9b80686d195f3effc528d4a1027be1b1d
Ibrahim.Ghazy <ibrahim@pioneers-solutions.com>
2020-10-01 09:21:08 +0200

    d

--- public/broadcast.js
+++ public/broadcast.js
@@ -2,7 +2,8 @@ const peerConnections = {};
 const config = {
   iceServers: [
     { 
-      "urls": "stun:stun.l.google.com:19302",
+      "urls": "stun4.l.google.com:19302",
+     
     },
     // { 
     //   "urls": "turn:TURN_IP?transport=tcp",
