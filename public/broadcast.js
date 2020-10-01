bda8b1c2476146ef93c6230ddcc19ddc361e15fe
Ibrahim.Ghazy <ibrahim@pioneers-solutions.com>
2020-10-01 12:34:14 +0200

    c

--- public/broadcast.js
+++ public/broadcast.js
@@ -1,11 +1,7 @@
 const peerConnections = {};
 const config = {
   iceServers: [
-    {urls: ["turn:173.194.72.127:19305?transport=udp",
-       "turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
-       "turn:173.194.72.127:443?transport=tcp",
-       "turn:[2404:6800:4008:C01::7F]:443?transport=tcp"
-       ],
+    {url: "turn:173.194.72.127:19305?transport=udp",       
      username:"CKjCuLwFEgahxNRjuTAYzc/s6OMT",
      credential:"u1SQDR/SQsPQIxXNWQT7czc/G4c="
     },
